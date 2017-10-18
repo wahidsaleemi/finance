c0347843d1d1f5c12f6e0f0deb8cb1eec9d7b6c3
wahidsaleemi <wahidsaleemi@users.noreply.github.com>
2017-10-18 10:39:41 -0500

    Added script ConvertTo-TWS.ps1
    This script converts VectorVest Watchlists (or a simple list of symbols) to Interactive Brokers compatible.

---
+++ ConvertTo-TWS.ps1
@@ -0,0 +1,18 @@
+﻿
+$inputFile = ".\vvsymbols.csv"
+$outputFile = ".\twssymbols.csv"
+
+#Get the VectorVest Watchlist
+$symbols = Get-Content $inputFile
+
+#Remove top row
+$symbols = ($symbols[2..($symbols.Count -1)])
+
+#Add "SYM" in first column in each row and "SMART/AMEX" as last column
+$symbols | 
+      Foreach-Object { 
+           $_ -replace $_, "SYM,$_,SMART/AMEX"
+  } | Set-Content( $outputFile ) -Force
+
+#Invoke-Expression $outputFile
+
